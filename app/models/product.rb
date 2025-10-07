c02cc5162d236921c70aa1814c2b34b3a4eda3a2
sukhdev <sukhdevrajput0076@gmail.com>
2025-10-07 10:38:05 +0530

    Validation!

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
 	validates :title, :description, :price, :image_url,  presence: true
 	validates :price, numericality: { greater_than_or_equal_to: 0.01 }
-	validates :title, uniqueness: true
+	validates :title, uniqueness: true, length: {minimum: 10, maximum: 30}
 	validates :image_url, allow_blank: true, format: {with: %r{\.(gif|jpg|png)\z}i, message: 'must be a URL for GIF, JPG or PNG image.'}
 end
